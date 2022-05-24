db45e57b12899ceaef97472ebeaca7ef7fed70ec
walid18 <walid@griffinkenya.com>
2022-05-24 21:22:43 +0100

    Core: sign-in/out set up

--- projects/shell/src/app/app.component.ts
+++ projects/shell/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Component, Inject } from '@angular/core';
+import { OktaAuthStateService, OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+import { filter, map, Observable, of, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +9,30 @@ import { Observable, of } from 'rxjs';
   styles: []
 })
 export class AppComponent {
-  public isAuthenticated$: Observable<boolean> = of(false);
-  public name$: Observable<string> = of('');
+  public isAuthenticated$: Observable<boolean> = this.oktaStateService.authState$
+      .pipe(
+          filter(authState => !!authState),
+          map(authState => authState.isAuthenticated ?? false),
+          shareReplay()
+      );
+  
+  public name$: Observable<string> = this.oktaStateService.authState$
+      .pipe(
+          filter(authState => !!authState && !!authState.isAuthenticated),
+          map(authState => authState.idToken?.claims.name ?? '')
+      );
 
-  constructor() { }
+  constructor(private oktaStateService: OktaAuthStateService, @Inject(OKTA_AUTH) private oktaAuth: OktaAuth) { }
+
+  public async signIn(): Promise<void> {
+    await this.oktaAuth.signInWithRedirect();
+  }
+
+  public async signOut(): Promise<void> {
+    await this.oktaAuth.signOut();
+  }
 }
+
+
+
 
