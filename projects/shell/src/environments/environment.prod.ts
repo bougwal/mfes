15072edf696eeec9c497b6e687cee357f12a1ee9
walid18 <walid@griffinkenya.com>
2022-05-26 22:30:10 +0100

    Core; environment vars for the MFEs deployment URI changes

--- projects/shell/src/environments/environment.prod.ts
+++ projects/shell/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true, 
   mfe: {
-    "mfeBasket": "https://mfes-basket.vercel.app/",
-    "mfeProfile": "https://mfes-profile.vercel.app/"
+    "mfeBasket": "https://mfes-basket.vercel.app",
+    "mfeProfile": "https://mfes-profile.vercel.app"
   }
 };
